0c26cf0bf26d7199c00d28bc4dc66549e39ae559
danny <danny.brabant@cegepmontpetit.ca>
2025-04-30 10:57:08 -0400

    TESTEE

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('real holaaaaaaaaaaaaa\n');
+    res.end('THIS IS A TEST OK!\n');
 });
 
 
