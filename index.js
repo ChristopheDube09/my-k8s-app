45ab6337416dae574536b0b2284d6233b8f2271e
danny <danny.brabant@cegepmontpetit.ca>
2025-04-30 10:51:29 -0400

    realll

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('holaaaaaaaa\n');
+    res.end('real hola\n');
 });
 
 
