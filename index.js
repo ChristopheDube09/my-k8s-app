5578bb0d42f0d3c3484c21cbd7288d4a4613b36c
danny <danny.brabant@cegepmontpetit.ca>
2025-04-30 09:58:27 -0400

    FINALETAPE4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('TEST TES TES TES TES TEST\n');
+    res.end('is it gonna work? final testd\n');
 });
 
 
