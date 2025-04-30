55e97c8829d7f4e78b9b95b3ef7777d0c296c8f3
danny <danny.brabant@cegepmontpetit.ca>
2025-04-30 11:03:18 -0400

    WHATSWRONG

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('final test ok thanks!\n');
+    res.end('WHATS WRONG MAN\n');
 });
 
 
